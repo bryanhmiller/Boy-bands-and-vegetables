6bdefd8ac71506aa2cdb910fd8d8bb2d7504893a
bryanhmiller <bryankalos@hotmail.com>
2017-02-10 19:49:31 -0600

    Linked boybands.js to index.html, added necessary javaScript to complete the assignment objectives.

--- boybands.js
+++ boybands.js
@@ -2,7 +2,7 @@ var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
 // The number of loops to perform (what if the array changes?)
-var loopCount = 5;
+var loopCount = bands.length;
 
 // Keep track of which band we're on in the loop
 var currentBand = "";
@@ -11,19 +11,23 @@ var currentBand = "";
 var currentVeggie = "";
 
 // Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
+var bandElement = document.getElementById("boy-bands");
 
 // Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
+var veggieElement = document.getElementById("vegetables");
 
 // Start looping
 for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
 
   // Add the band names into the correct <div>
-  currentBand = ???;
-
+  currentBand = bandElement.innerHTML += "<div>" + bands[loopTracker] + "</div>";
+  	
 
   // Add the veggie names into the correct <div>
-  currentVeggie = ???;
+  currentVeggie = veggieElement.innerHTML += "<div>" + vegetables[loopTracker] + "</div>";
 
 }
+
+// Loop through the two arrays provided (bands and vegetables) and output each 
+// element in the arrays into their corresponding HTML <div> element. Ensure that 
+// each item is in a block element (e.g. li, div, p. etc...)
